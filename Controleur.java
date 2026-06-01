21850274079a75672fbfa018f748a0095f01af9b
Axel Douillard <axel.douillard@etu.univ-lehavre.fr>
2026-06-01 14:34:06 +0200

    controleur v2

---
+++ Controleur.java
@@ -0,0 +1,16 @@
+import IHM.*;
+
+public class Controleur 
+{
+	private FrameMenu frameMenu;
+	
+	public Controleur()
+	{
+		this.frameMenu = new FrameMenu();
+	}
+	
+	public static void main(String[] args) 
+	{
+		Controleur controleur = new Controleur();
+	}
+}
