424c3da106da16873545412ce0986afaa4d9b563
Axel Douillard <axel.douillard@etu.univ-lehavre.fr>
2026-06-01 15:22:51 +0200

    PanelMenu avec plateau

--- IHM/PanelMenu.java
+++ IHM/PanelMenu.java
@@ -2,21 +2,26 @@ package IHM;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import Controleur;
 
 public class PanelMenu extends JPanel implements ActionListener
 {
 	private JButton buttonSolo;
 	private JButton buttonCreation;
-	private JComboBox<Plateau> jcbPlateaux;
+	private JComboBox jcbPlateaux;
+	private Controleur ctrl;
 
 	public PanelMenu()
 	{
+		this.ctrl = ctrl;
 		this.setLayout(new GridLayout(5, 1));
 
-		this.jcbPlateaux = new JComboBox<Plateau>();
+		this.jcbPlateaux = new JComboBox(this.ctrl.getNomPlateaux());
+
+		this.jcbPlateaux = new JComboBox<String>();
 		for (Plateau plateau : Plateau.values()) 
 		{
-			this.jcbPlateaux.addItem(plateau);
+			this.jcbPlateaux.addItem(plateau.toString());
 	    }
 		
 		this.buttonSolo = new JButton("Solo");
@@ -36,8 +41,8 @@ public class PanelMenu extends JPanel implements ActionListener
 	{
 		if (e.getSource() == this.buttonSolo) 
 		{
-			Plateau plateauSelectionne = (Plateau) this.jcbPlateaux.getSelectedItem();
-			new FrameSolo(plateauSelectionne);
+			String plateauSelectionne = (String) this.jcbPlateaux.getSelectedItem();
+			new FrameSolo(Plateau.valueOf(plateauSelectionne));
 		} 
 		else if (e.getSource() == this.buttonCreation) 
 		{
