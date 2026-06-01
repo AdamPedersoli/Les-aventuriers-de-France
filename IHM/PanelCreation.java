4f6fa3f454d74017cd59691a5f071fe4f8a5f77e
Axel Douillard <axel.douillard@etu.univ-lehavre.fr>
2026-06-01 17:10:03 +0200

    PanelMenu avec plateau

--- IHM/PanelCreation.java
+++ IHM/PanelCreation.java
@@ -1,5 +1,54 @@
+package IHM;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import Controleur;
 
+public class PanelCreation extends JPanel
+{
+	// panel Principal
+	private JPanel panelConfig;
+	private JPanel panelGrille;
+	private Controleur ctrl;
 
-public class PanelCreation {
-	
+	public PanelCreation()
+	{
+		this.setLayout(new GridLayout(1, 2));
+		this.panelConfig = new JPanel();
+		this.panelGrille = new JPanel();
+		this.panelConfig.setLayout(new GridLayout(3,1));
+		this.panelGrille.setLayout(new GridLayout(3,1));
+		
+		JPanel panelPoles      = new JPanel();
+		JPanel panelDep        = new JPanel();
+		JPanel panelTransport  = new JPanel();
+		JPanel panelNomPlateau = new JPanel();
+		JPanel panelVisual     = new JPanel();
+		JPanel panelBouton     = new JPanel();
+
+		JComboBox jcbPoles = new JComboBox(this.ctrl.getNomPoles());
+		JComboBox jcbDep   = new JComboBox(this.ctrl.getNomDeps());
+		JComboBox jcbTrans = new JComboBox(this.ctrl.getNomTransports());
+
+		panelPoles.add(jcbPoles);
+		panelDep.add(jcbDep);
+		panelTransport.add(jcbTrans);
+
+		this.panelConfig.add(panelPoles);
+		this.panelConfig.add(panelDep);
+		this.panelConfig.add(panelTransport);
+
+		JTextField tfNomPlateau = new JTextField("Nom du plateau", 20);
+		JButton buttonVisualiser = new JButton("Visualiser");
+
+		panelNomPlateau.add(tfNomPlateau);
+		panelVisual.add(buttonVisualiser);
+
+		this.panelGrille.add(panelNomPlateau);
+		this.panelGrille.add(panelVisual);
+		this.panelGrille.add(panelBouton);
+
+		this.add(this.panelConfig);
+		this.add(this.panelGrille);
+	}
 }
