643057bc8c54d6f0c7abc9e22b724fb5d660949e
Lubin VOM HOEVEL <lubin.vom-hoevel@etu.univ-lehavre.fr>
2026-06-10 08:56:12 +0200

    Récuperation des fichiers de conception ainsi que du metier du jeu

--- Jeu/Metier/Case.java
+++ Jeu/Metier/Case.java
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-package Metier;
-
-public class Case
-{
-	private Pole pole;
-	private int  posX;
-	private int  posY;
-	
-	public Case( int posX, int posY )
-	{
-		this( null, posX, posY )		
-	}
-	
-	public Case( Pole p, int posX, int posY )
-	{
-		this.pole = pole;
-		this.posX = posX;
-		this.posY = posY;
-	}
-	
-	public int 	getX() 		{ return this.posX; }
-	public int 	getY() 		{ return this.posY; }
-	public Pole getPole() 	{ return this.pole; }
-}
-=======
 package Jeu.Metier;
 
 import java.awt.Point;
@@ -114,4 +88,3 @@ public class Case
 	}
 	
 }
->>>>>>> main
